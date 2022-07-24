573deff7cc77d71b63d9676284b2de10fbe187fb
Shogo Ogami <sg.tao.so@gmail.com>
2022-07-24 19:52:05 +0900

    update 11_vue-create for build template of DcHeros

--- 11_vue-create-app/src/views/DcHeros.vue
+++ 11_vue-create-app/src/views/DcHeros.vue
@@ -3,16 +3,24 @@
   <h1>DcHeros</h1>
   <ul>
     <li v-for="(hero, index) in dcHeros" :key="index">
-      {{ index }}: {{ hero.name }}
+      <div>
+        {{ index }}: {{ hero.name }}
+        <button>x</button>
+      </div>
     </li>
   </ul>
+  <input v-bind:value="newHero.data" />
+  <button v-bind:disabled="isDisabled">Add Hero</button>
 </template>
 <script>
 export default {
   data() {
     return {
+      isDisabled: false,
+      newHero: { data: 'Aquaman' },
       dcHeros: [{ name: 'SuperGirl' }, { name: 'Flash' }, { name: 'Batman' }, { name: 'Arrow' }, { name: 'SuperMan' }],
     }
   },
 }
 </script>
+<!-- EOF -->
