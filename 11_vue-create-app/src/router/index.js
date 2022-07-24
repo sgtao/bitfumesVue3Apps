3cffc654b7b1599ddc63b3f88b16ae7d013d44df
Shogo Ogami <sg.tao.so@gmail.com>
2022-07-24 15:29:09 +0900

    update 11_vue-create for add page of Dcheros at first

--- 11_vue-create-app/src/router/index.js
+++ 11_vue-create-app/src/router/index.js
@@ -1,7 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import defaultHome from '../views/defaultHome.vue'
 import defaultAbout from '../views/defaultAbout.vue'
-
+import DcHeros from '../views/DcHeros.vue'
+//
 const routes = [
   {
     path: '/',
@@ -13,11 +14,18 @@ const routes = [
     name: 'defaultAbout',
     component: defaultAbout,
   },
+  {
+    path: '/DcHeros',
+    name: 'DcHeros',
+    component: DcHeros,
+  },
 ]
-
+//
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
 })
-
+//
 export default router
+//
+// EOF
---
+++ 11_vue-create-app/src/views/DcHeros.vue
@@ -0,0 +1,18 @@
+<!-- DcHeros.vuee -->
+<template>
+  <h1>DcHeros</h1>
+  <ul>
+    <li v-for="(hero, index) in dcHeros" :key="index">
+      {{ index }}: {{ hero.name }}
+    </li>
+  </ul>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      dcHeros: [{ name: 'SuperGirl' }, { name: 'Flash' }, { name: 'Batman' }, { name: 'Arrow' }, { name: 'SuperMan' }],
+    }
+  },
+}
+</script>
