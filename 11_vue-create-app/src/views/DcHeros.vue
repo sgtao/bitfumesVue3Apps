04fc6098f457adee55490676550f9548733fb663
Shogo Ogami <sg.tao.so@gmail.com>
2022-07-27 01:18:24 +0900

    update 11_vue-create for dcHeros app showing lists

--- 11_vue-create-app/src/views/DcHeros.vue
+++ 11_vue-create-app/src/views/DcHeros.vue
@@ -1,25 +1,48 @@
 <!-- DcHeros.vuee -->
 <template>
-  <h1>DcHeros</h1>
-  <ul>
-    <li v-for="(hero, index) in dcHeros" :key="index">
-      <div>
-        {{ index }}: {{ hero.name }}
-        <button>x</button>
-      </div>
-    </li>
-  </ul>
-  <input v-bind:value="newHero.data" />
-  <button v-bind:disabled="isDisabled">Add Hero</button>
+  <div>
+    <h1>Dc Heros herosCount {{ herosCount }}</h1>
+    <ul>
+      <li v-for="(hero, index) in dcHeros" :key="hero.name">
+        <div>
+          {{ index }}: {{ hero.name }}
+          <button>x</button>
+        </div>
+      </li>
+    </ul>
+    <form>
+      <input v-model="newHero" ref="newHeroRef" />
+      <button>Add Hero</button>
+    </form>
+  </div>
 </template>
 <script>
+import { computed, onMounted, ref } from "vue";
 export default {
-  data() {
-    return {
-      isDisabled: false,
-      newHero: { data: 'Aquaman' },
-      dcHeros: [{ name: 'SuperGirl' }, { name: 'Flash' }, { name: 'Batman' }, { name: 'Arrow' }, { name: 'SuperMan' }],
-    }
+  setup() {
+    // newHeroRef: 初回レンダリング時(mountされたとき)にfocusするデータ
+    const newHeroRef = ref("");
+    // newHero: v-modelでinput要素と連動するデータ
+    const newHero = ref("");
+    // dcHeros: リストデータ
+    const dcHeros = ref([
+      { name: "SuperGirl" },
+      { name: "Flash" },
+      { name: "Batman" },
+      { name: "Arrow" },
+      { name: "SuperMan" }
+    ]);
+    // mountされたとき、input要素にforcusする？
+    onMounted(() => {
+      newHeroRef.value.focus();
+      console.log(dcHeros.value);
+    });
+    // heroCount: dcHerosオブジェクトのアイテム数をカウントする
+    const herosCount = computed({
+      get: () => dcHeros.value.length,
+    });
+    //
+    return { newHeroRef, newHero, dcHeros, herosCount };
   },
 }
 </script>
