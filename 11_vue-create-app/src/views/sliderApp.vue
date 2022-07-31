0d522f05e07740236d38e2ca04fb8b1487c5095a
Shogo Ogami <sg.tao.so@gmail.com>
2022-07-31 23:13:57 +0900

    update 11_vue-create for append transition style to Slider App

--- 11_vue-create-app/src/views/sliderApp.vue
+++ 11_vue-create-app/src/views/sliderApp.vue
@@ -3,35 +3,60 @@
 <template>
     <section class="flex flex-wrap w-full relative">
         <h1 class="m-auto w-full text-xl">Slider</h1>
-        <div class="w-full" v-for="(color, index) in sliders" :key="color">
-            <div
-            :class="color"
-            style="height:350px"
-            >{{ index }}</div>            
+        <div class="w-full absolute" v-for="(color, index) in sliders" :key="color">
+            <transition name="fade">
+                <div v-if="index === currentSlide"
+                :class="color" style="height: 350px">{{ index }}</div>
+            </transition>
         </div>
     </section>
 </template>
 <!-- script -->
 <script>
-import { ref, reactive } from 'vue';
+import {ref, reactive, onMounted, onBeforeUnmount} from 'vue';
 export default {
     setup() {
         const currentSlide = ref(1); // 表示スライド番号
-        const sliders = reactive([ // スライドcolor
-            "bg-teal-600",
-            "bg-blue-600",
-            "bg-yellow-600",
+        const sliders = reactive([
+            // スライドcolor
+            'bg-teal-600',
+            'bg-blue-600',
+            'bg-yellow-600',
         ]);
+        const interval = ref("");
+        //
+        // define function as method 
         const makeActive = (index) => {
             currentSlide.value = index;
-        }
+        };
+        //
+        // define lifecyle hooks
+        onMounted(() => {
+            interval.value = setInterval(() => {
+                currentSlide.value = 
+                    currentSlide.value === 2 ? 0 : 
+                    currentSlide.value += 1;
+                }, 2000);
+        });
+        onBeforeUnmount(() => {
+            clearInterval(interval.value);
+        });
         // exported variables
         return {
-            currentSlide, sliders, makeActive,
-        }
-    }
-}
+            currentSlide,
+            sliders,
+            makeActive,
+        };
+    },
+};
 </script>
 <!-- style -->
-<style></style>
+<style>
+    .fade-enter-from {
+        opacity: 0;
+    }
+    .fade-leave-to {
+        opacity: 0;
+    }
+</style>
 <!-- EOF -->
