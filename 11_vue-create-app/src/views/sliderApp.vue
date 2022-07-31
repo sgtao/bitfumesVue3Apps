bf17ef23904861e635067a60a8f9844c31ea1d69
Shogo Ogami <sg.tao.so@gmail.com>
2022-07-31 22:59:55 +0900

    update 11_vue-create for show all slides of Slider App

--- 11_vue-create-app/src/views/sliderApp.vue
+++ 11_vue-create-app/src/views/sliderApp.vue
@@ -1,12 +1,37 @@
 <!-- sliderApp.vue -->
 <!-- template -->
 <template>
-    <section class="m-auto">
-        <h1>Slider</h1>
+    <section class="flex flex-wrap w-full relative">
+        <h1 class="m-auto w-full text-xl">Slider</h1>
+        <div class="w-full" v-for="(color, index) in sliders" :key="color">
+            <div
+            :class="color"
+            style="height:350px"
+            >{{ index }}</div>            
+        </div>
     </section>
 </template>
 <!-- script -->
-<script></script>
+<script>
+import { ref, reactive } from 'vue';
+export default {
+    setup() {
+        const currentSlide = ref(1); // 表示スライド番号
+        const sliders = reactive([ // スライドcolor
+            "bg-teal-600",
+            "bg-blue-600",
+            "bg-yellow-600",
+        ]);
+        const makeActive = (index) => {
+            currentSlide.value = index;
+        }
+        // exported variables
+        return {
+            currentSlide, sliders, makeActive,
+        }
+    }
+}
+</script>
 <!-- style -->
 <style></style>
 <!-- EOF -->
