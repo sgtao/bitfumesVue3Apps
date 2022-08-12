3e95cb5162551b3f52d80baa19220389ab770d24
Shogo Ogami <sg.tao.so@gmail.com>
2022-08-12 19:47:10 +0900

    update 11_vue-create for refactor CalendarApp by emit

--- 11_vue-create-app/src/components/MonthCalendar/PageButtons.vue
+++ 11_vue-create-app/src/components/MonthCalendar/PageButtons.vue
@@ -1,21 +1,29 @@
 <!-- pageButtons.vue -->
 <template>
     <section class="flex justify-between">
-        <button class="px-2 border rounded bg-red-200" v-on:click="$emit('emitPrevMonth')">
-            prev
-        </button>
-        <button class="px-2 border rounded bg-green-200" v-on:click="$emit('emitCurrMonth')">
-            today
-        </button>
-        <button class="px-2 border rounded bg-blue-200" v-on:click="$emit('emitNextMonth')">
-            next
-        </button>
+        <button class="px-2 border rounded bg-red-200" v-on:click="prevMonth">prev</button>
+        <button class="px-2 border rounded bg-green-200" v-on:click="currMonth">today</button>
+        <button class="px-2 border rounded bg-blue-200" v-on:click="nextMonth">next</button>
     </section>
 </template>
 <script>
 export default {
     name: 'pageButtons',
-    emits: ['emitPrevMonth', 'emitCurrMonth', 'emitNextMonth'],
+    setup(_, {emit}) {
+        /**
+         * emit functions
+         */
+        const prevMonth = () => {
+            emit('emitPrevMonth');
+        };
+        const currMonth = () => {
+            emit('emitCurrMonth');
+        };
+        const nextMonth = () => {
+            emit('emitNextMonth');
+        };
+        return {prevMonth, currMonth, nextMonth};
+    },
 };
 </script>
 <!-- EOF -->
